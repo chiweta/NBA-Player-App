62c34a21d589fa6eeaf59f4883ed9bc513b59258
Chiweta <chiweta2211@gmail.com>
2024-01-19 17:25:04 -0500

    added springboot

---
+++ src/sb/src/test/java/userinterface/example/sb/SbApplicationTests.java
@@ -0,0 +1,13 @@
+package userinterface.example.sb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
